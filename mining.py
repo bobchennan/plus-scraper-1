7e9a2d3aa90e3ce331f27f83e0ea95a7c07f7b6d
whtsky <whtsky@vip.qq.com>
2012-04-21 21:59:57 +0800

    use find all.

--- mining.py
+++ mining.py
@@ -4,7 +4,7 @@ import re
 PATH = 'data/' #Where to save sitemap txts.
 count = 0
 finder = re.compile('<loc>(.*?)</loc>')
-for f in finder.finditer(urlopen('http://www.gstatic.com/s2/sitemaps/profiles-sitemap.xml').read()):
+for f in finder.findall(urlopen('http://www.gstatic.com/s2/sitemaps/profiles-sitemap.xml').read()):
 	txt = urlopen(f).read()
 	count += len(txt.split('\n'))
 	open(''.join([PATH, f.split('/')[-1]]), 'w').write(txt)
