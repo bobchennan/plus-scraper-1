65769de0903c994fe7073db710128500adb140af
whtsky <whtsky@vip.qq.com>
2012-04-21 21:52:32 +0800

    first commit

---
+++ mining.py
@@ -0,0 +1,12 @@
+from urllib import urlopen
+import re
+
+PATH = 'data/' #Where to save sitemap txts.
+count = 0
+finder = re.compile('<loc>(.*?)</loc>')
+for f in finder.finditer(urlopen('http://www.gstatic.com/s2/sitemaps/profiles-sitemap.xml').read()):
+	txt = urlopen(f).read()
+	count += len(txt.split('\n'))
+	open(''.join([PATH, f.split('/')[-1]]), 'w').write(txt)
+
+print '%s users found.' % count
